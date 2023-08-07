2bfbd91d7dbf9af2a228676d432dd1fc7e339376
b-gilley <141060801+b-gilley@users.noreply.github.com>
2023-08-07 18:52:06 +0100

    Made edits to site's metadata file.

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Benjamin Gilley",
+		email: "benji.gilley01@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
